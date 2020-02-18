a552ec5f40b431f250164383cf86f557000a4485
ItDidntCatchFire <deaddanster@googlemail.com>
2020-02-18 02:11:14 +0000

    Added Create, Read and Delete for UserDatabaseAccess

--- DistSysACW/DistSysACW/Models/User.cs
+++ DistSysACW/DistSysACW/Models/User.cs
@@ -21,15 +21,12 @@ namespace DistSysACW.Models
         public string UserName { get; set; }
         public Roles Role { get; set; }
 
-
         public User()
         {
 
         }
         #endregion
     }
-
-
 
     #region Task13?
     // TODO: You may find it useful to add code here for Logging
@@ -39,8 +36,46 @@ namespace DistSysACW.Models
     {
         #region Task3 
         // TODO: Make methods which allow us to read from/write to the database 
+
+        public static async Task<User> NewUserAsync(string userName, UserContext context)
+        {
+            var user = new User()
+            {
+                ApiKey = Guid.NewGuid().ToString(),
+                UserName = userName,
+                Role = User.Roles.User
+            };
+
+            context.Users.Add(user);
+            await context.SaveChangesAsync();
+
+            return user;
+        }
+
+        //https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/statements-expressions-operators/expression-bodied-members
+        public static async Task<bool> UserExistsAsync(string apiKey, UserContext context)
+            => GetUserByApiKeyAsync(apiKey, context) == null;
+
+        public static async Task<bool> UserExistsAsync(string apiKey, string userName, UserContext context)
+        {
+            var user = context.Users.FirstOrDefault(a => a.ApiKey == apiKey && a.UserName == userName);
+
+            return user == null;
+        }
+
+        public static async Task<User> GetUserByApiKeyAsync(string apiKey, UserContext context)
+        {
+            var user = context.Users.FirstOrDefault(a => a.ApiKey == apiKey);
+
+            return user;
+        }
+
+        public static async Task DeleteUserAsync(string apiKey, UserContext context)
+        {
+            context.Remove(GetUserByApiKeyAsync(apiKey, context));
+            await context.SaveChangesAsync();
+        }
+
         #endregion
     }
-
-
 }
