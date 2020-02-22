13eb10ffd130ff6969cd6a11ab80389948285c82
ItDidntCatchFire <deaddanster@googlemail.com>
2020-02-22 01:00:02 +0000

    Task5 Complete

--- DistSysACW/DistSysACW/Middleware/AuthMiddleware.cs
+++ DistSysACW/DistSysACW/Middleware/AuthMiddleware.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -15,11 +16,28 @@ namespace DistSysACW.Middleware
             _next = next;
         }
 
-        public async Task InvokeAsync(HttpContext context, Data.UserContext dbContext)
+        public async Task InvokeAsync(HttpContext context, Data.IUserRepository userRepository)
         {
             #region Task5
             // TODO:  Find if a header ‘ApiKey’ exists, and if it does, check the database to determine if the given API Key is valid
             //        Then set the correct roles for the User, using claims
+
+            if (context.Request.Headers.TryGetValue("Api Key", out var apiKey)) {
+                var user = await userRepository.GetByIdAsync(apiKey);
+
+                var claims = new List<Claim>()
+                {
+                    new Claim(ClaimTypes.Name, user.UserName),
+                    new Claim(ClaimTypes.Role, nameof(user.Role))
+                };
+
+                ClaimsIdentity cli = new ClaimsIdentity(claims, "ApiKey");
+                cli.AddClaims(claims);
+
+                context.User.AddIdentity(cli);
+            }
+
+
             #endregion
 
             // Call the next delegate/middleware in the pipeline
